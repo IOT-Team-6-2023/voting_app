2c4df7930a26491ccd997db469709ca0c87617c0
deeptadevkota <deeptadevkota21@gmail.com>
2023-04-02 01:26:48 +0530

    get-template-completed

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('candidateList/', views.candidate_list, name='candidate_list')
 ]
--- vote_app/views.py
+++ vote_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'home.html', {'name':'Deepta'})
+
+def candidate_list(request):
+    response = requests.get('https://api.covid19api.com/countries').json()
+    return render(request,'candidate_list.html', {'response': response})
