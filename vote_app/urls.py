b98d8c2784a16f0c7f99819ab3ff5d868bf52a3a
deeptadevkota <deeptadevkota21@gmail.com>
2023-04-02 23:27:36 +0530

    post-template-done

--- vote_app/urls.py
+++ vote_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('candidateList/', views.candidate_list, name='candidate_list')
+    path('candidateList/', views.candidate_list, name='candidate_list'),
+    path('voteCandidate/', views.vote_candidate, name='vote_candidate'),
+    path('votingAction/', views.votingAction, name='vote_candidate'),
 ]
--- vote_app/views.py
+++ vote_app/views.py
@@ -11,3 +11,15 @@ def index(request):
 def candidate_list(request):
     response = requests.get('https://api.covid19api.com/countries').json()
     return render(request,'candidate_list.html', {'response': response})
+
+def vote_candidate(request):
+    return render(request,'vote_page.html', {})
+
+def votingAction(request):
+    candidate_id = request.POST.get("candidate_id")
+    constituency_id = request.POST.get('constituency_id')
+    post_data = {'candidate_id':candidate_id, 'constituency_id':constituency_id }
+    # api_response = requests.post(url = "api_endpoint", data = post_data )
+    return render(request, 'vote_confirm.html',post_data)
+
+# {'candidate_id': candidate_id, 'constituency_id': constituency_id}
