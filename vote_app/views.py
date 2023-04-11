ea4ff670dc06c38121d2bc288d4ca767d6bf6493
deeptadevkota <deeptadevkota21@gmail.com>
2023-04-11 14:31:00 +0530

    final-changes

--- vote_app/views.py
+++ vote_app/views.py
@@ -4,12 +4,14 @@ import requests
 
 from django.http import HttpResponse
 
+BASE = "http://192.168.196.59:3000"
+
 
 def index(request):
     return render(request,'home.html')
 
 def candidate_list(request):
-    api_end_point = ""
+    api_end_point = BASE + "/candidates"
     response = requests.get(api_end_point).json()
     return render(request,'candidate_list.html', {'response': response})
 
@@ -18,7 +20,7 @@ def vote_candidate(request):
 
 def votingAction(request):
     candidate_id = request.POST.get("candidate_id")
-    # constituency_id = request.POST.get('constituency_id')
     post_data = {'candidate_id':candidate_id }
-    # api_response = requests.post(url = "api_endpoint", data = post_data )
+    api_response = requests.post(url = BASE +"/votes", json = post_data )
+    print(api_response)
     return render(request, 'vote_confirm.html',post_data)
